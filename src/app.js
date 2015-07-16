3746f3bb9741ff471d16fc59e7262659cfdfe29e
Mattias Petter Johansson <mpj@spotify.com>
2015-07-16 12:29:52 +0200

    Basic react example

--- src/app.js
+++ src/app.js
@@ -2,16 +2,45 @@ import $ from 'jquery'
 import React from 'react'
 import 'babelify/polyfill'
 
+export class MyListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stuff: [],
+      loading: false
+    }
+
+    this.loadStuff = () => {
+      this.setState({ loading: true })
+      fetch('http://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => this.setState({
+          stuff: json,
+          loading: false
+        }))
+        .catch(err => console.warn('Failed fetching from endpoint', err))
+    }
+
+    this.render = () =>
+      <div>
+        <button onClick={this.loadStuff}>
+          {this.state.loading ? 'Loading...' : 'Click me to load stuff'}
+        </button>
+        {this.state.stuff.map(x =>
+          <div key={x.id}>{x.title}</div>
+        )}
+      </div>
+  }
+
+}
+
+
+
 $().ready(() => {
 
-  fetch('http://jsonplaceholder.typicode.com/psosts')
-    .then(response => response.json())
-    .then(json => console.log("response", json))
-    .catch(err => console.warn('Failed fetching from endpoint', err))
+
 
   let target = document.getElementById('render-target')
-  React.render(
-    <h1>Hello, master</h1>,
-    target
-  )
+  React.render(<MyListComponent />, target)
 })
