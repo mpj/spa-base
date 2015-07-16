7414ef2199302099ef042a3e05a9f49c85442312
Mattias Petter Johansson <mpj@spotify.com>
2015-07-16 11:55:37 +0200

    fetch example

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import $ from 'jquery'
 import React from 'react'
+import 'babelify/polyfill'
 
 $().ready(() => {
+
+  fetch('http://jsonplaceholder.typicode.com/psosts')
+    .then(response => response.json())
+    .then(json => console.log("response", json))
+    .catch(err => console.warn('Failed fetching from endpoint', err))
+
   let target = document.getElementById('render-target')
   React.render(
-    <h1>Hello, </h1>,
+    <h1>Hello, master</h1>,
     target
   )
 })
